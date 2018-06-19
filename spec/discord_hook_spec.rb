be34d90d081eb5d6a5d54b2d87f677b4cf05dfd0
Patrick Auernig <patrick.auernig@mykolab.com>
2018-06-19 20:44:37 +0200

    Add tests for outgoing Discord merge request hook

--- spec/discord_hook_spec.rb
+++ spec/discord_hook_spec.rb
@@ -26,4 +26,53 @@ RSpec.describe DiscordHooks do
       expect(described_class.push_hook(gl_push)).to eq(expected)
     end
   end
+
+  context "merge hook" do
+    let(:gl_mr_open) { objectified_fixture("gl_mr_open.json") }
+    let(:gl_mr_close) do
+      gl_mr_open.dup.tap do |x|
+        x.object_attributes.action = "close"
+        x.object_attributes.state = "closed"
+      end
+    end
+    let(:gl_mr_merge) do
+      gl_mr_open.dup.tap do |x|
+        x.object_attributes.action = "merge"
+        x.object_attributes.state = "merged"
+      end
+    end
+    let(:expected_open) do
+      {
+        author: { name: "Testmaster", icon_url: "http://example.com/testmaster.png" },
+        title: "Project - Merge request opened: !4 Implement anti-cheat system",
+        url: "https://gitlab.com/testmaster/project/merge_requests/4",
+        description: "Add a anti-cheat system to keep those cheaters in check.\nAuto-ban included!",
+        color: 0xE24329,
+        footer: { text: "testmaster/project", icon_url: "https://gitlab.com/testmaster/project/avatar.png" },
+        timestamp: Time.parse("2018-06-19 12:28:46 UTC").iso8601
+      }
+    end
+    let(:expected_close) do
+      expected_open.dup.tap do |x|
+        x[:title] = "Project - Merge request closed: !4 Implement anti-cheat system"
+      end
+    end
+    let(:expected_merge) do
+      expected_open.dup.tap do |x|
+        x[:title] = "Project - Merge request merged: !4 Implement anti-cheat system"
+      end
+    end
+
+    it "creates an embed on opened requests" do
+      expect(described_class.merge_request_hook(gl_mr_open)).to eq(expected_open)
+    end
+
+    it "creates an embed on closed requests" do
+      expect(described_class.merge_request_hook(gl_mr_close)).to eq(expected_close)
+    end
+
+    it "creates an embed on merged requests" do
+      expect(described_class.merge_request_hook(gl_mr_merge)).to eq(expected_merge)
+    end
+  end
 end
