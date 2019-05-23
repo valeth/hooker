17b8f7dcd1ac65003fd38e86c6f8e449eb203050
Patrick Auernig <patrick.auernig@mykolab.com>
2019-05-23 14:59:03 +0200

    Add default routes, and content type

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "dotenv"
 
 Dotenv.load
@@ -6,6 +8,10 @@ require "sinatra"
 require_relative "notification_worker"
 
 class App < Sinatra::Application
+  before do
+    content_type "application/json"
+  end
+
   post "/gitlab" do
     token = request.get_header("HTTP_X_GITLAB_TOKEN")
     event = request.get_header("HTTP_X_GITLAB_EVENT")
@@ -14,5 +20,20 @@ class App < Sinatra::Application
     else
       status 400
     end
+  end
+
+  get "*.php" do
+    status 418
+    body JSON.generate(error: "PHP")
+  end
+
+  # Fallback routes
+
+  get "*" do
+    status 400
+  end
+
+  post "*" do
+    status 400
   end
 end
